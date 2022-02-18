be83cb769a448ad95d0059c873fb9d752b884fd7
Michiel Willems <91065562+pgm-michielwillems@users.noreply.github.com>
2022-02-18 18:35:49 +0100

    fixed logo and favicons

---
+++ public/scripts/btn-totop.js
@@ -0,0 +1,19 @@
+
+// $btn = document.getElementById('btn-totop');
+// $main = document.querySelector('.main-container');
+// $main.addEventListener('scroll', scrollDown);
+// $btn.addEventListener('click', toTop);
+
+// function scrollDown() {
+//   if ($main.scrollTop > 100) {
+//     $btn.style.right = 'calc(5.5rem - 2px)';
+//     $btn.style.opacity = '1';
+//   } else {
+//     $btn.style.right = '-10rem';
+//     $btn.style.opacity = '0';
+//   }
+// }
+
+// function toTop() {
+//   $main.scrollTop = 0;
+// }
---
+++ readme.md
@@ -0,0 +1,6 @@
+17 feb 2022
+
+'To Do IT!' webapplication made by Michiel Willems of 1PGMa for Artevelde University of Applied Sciences for the course Advanced Programming.
+
+About
+Small app with possibility to add tasks to a list using GET, POST, PUT, PATCH methods with data stored on a selfprogrammed server using Node.JS.
