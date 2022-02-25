7dedaa55c293ce150faa63dc7b4880816eba5932
Michiel Willems <91065562+pgm-michielwillems@users.noreply.github.com>
2022-02-25 21:21:31 +0100

    added themeswitch, works

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,47 +1,49 @@
-import { themes } from './themes.js';
-
+import { themes } from "./themes.js";
 (() => {
     const app = {
         init() {
+            // Get defined themes from themes module
+            this.themes = themes;
+            this.root = document.documentElement;
+            
+            // Set active theme get from local storage
+            this.activeThemeName = localStorage.getItem("activeThemeName") || "light";
             this.cacheElements();
-            this.generateUi();
-            setInterval(() => {
-                this.generateRandomTask()
-            }, 2000);
+            this.registerListeners();
+
+            // Populate dropdown box
+            this.populateSelect();
+
+            // Change theme, based on active theme
+            this.changeDOMTheme();
         },
         cacheElements() {
-            this.$input = document.querySelector('.input');
-            this.$switch = document.querySelector('#checkbox');
+            this.$selectSwitch = document.querySelector('#theme-switcher');
         },
-        generateUi() {
-            const root = document.documentElement;
-            this.$switch.addEventListener('click', () => {
-                root.style.setProperty('--primary-pink', '#00093a');
-                root.style.setProperty('--primary-yellow', '#7556e4');
-                root.style.setProperty('--primary-text', '#FFFFFF');
-                root.style.setProperty('--primary-black', '#000000');
-            })
+        registerListeners() {
+            this.$selectSwitch.addEventListener("change", (e) => {
+                // Change active theme here
+                this.activeThemeName = e.target.value;
+                // Change in local storage
+                localStorage.setItem("activeThemeName", e.target.value);
+                // Other theme, so change the dom colors
+                this.changeDOMTheme();
+            });
         },
-        generateRandomTask() {
-            const tasksArray = [
-                "Travel to another country",
-                "Break a bone",
-                "Win a contest",
-                "Ride a bike",
-                "Be on TV",
-                "Meet a celebrity",
-                "Built a piece of furniture",
-                "Fix my car",
-                "Go skydiving",
-                "Climb a mountain",
-                "Write a book",
-                "Have a car accident",
-                "See my favorite band in concert",
-                "Meet Kim Jong-Un"
-            ]
-            randomTask = Math.floor(Math.random() * tasksArray.length);
-            this.$input.placeholder = tasksArray[randomTask];
+        populateSelect() {
+            const options = themes.map((theme) => {
+                return `<option value="${theme.slug}" ${theme.slug == this.activeThemeName ? "selected" : ""}>${theme.name}</option>`
+            });
+            this.$selectSwitch.innerHTML = options.join('');
+        },
+        changeDOMTheme() {
+            const activeTheme = this.themes.find((theme) => theme.slug == this.activeThemeName);
+
+            activeTheme.colors.forEach((color) => {
+                this.root.style.setProperty(`--${color.name}`, color.hex);
+            });
         }
+        
     };
     app.init();
 })()
--- public/scripts/themes.js
+++ public/scripts/themes.js
@@ -1,22 +1,46 @@
 export const themes = [
     {
-        name: 'light',
-        slug: 'light-theme',
+        name: 'beach',
+        slug: 'beach',
         colors: [
-            {name:'primary-pink', hex: '#cc38b8'},
-            {name:'primary-yellow', hex: '#bd8c22'},
+            {name:'primary-top', hex: '#cc38b8'},
+            {name:'primary-bottom', hex: '#bd8c22'},
             {name:'primary-text', hex: '#FFFFFF'},
-            {name:'primary-border', hex: '#8b267e'}
+            {name:'primary-border', hex: '#8b267e'},
+            {name:'primary-buttons', hex: '#181818'},
         ]
     },
     {
-        name: 'dark',
-        slug: 'dark-theme',
+        name: 'midnight',
+        slug: 'midnight',
         colors: [
-            {name:'primary-pink', hex: '#00093a'},
-            {name:'primary-yellow', hex: '#7556e4'},
+            {name:'primary-top', hex: '#00093a'},
+            {name:'primary-bottom', hex: '#7556e4'},
             {name:'primary-text', hex: '#FFFFFF'},
-            {name:'primary-border', hex: '#000000'}
+            {name:'primary-border', hex: '#000000'},
+            {name:'primary-buttons', hex: '#000000'},
         ]
-    }
+    },
+    {
+        name: 'toxic',
+        slug: 'toxic',
+        colors: [
+            {name:'primary-top', hex: '#1A6566'},
+            {name:'primary-bottom', hex: '#5D9A66'},
+            {name:'primary-text', hex: '#ffff00'},
+            {name:'primary-border', hex: '#21445B'},
+            {name:'primary-buttons', hex: '#45214A'},
+        ]
+    },
+    {
+        name: 'unicorn',
+        slug: 'unicorn',
+        colors: [
+            {name:'primary-top', hex: '#0011ff'},
+            {name:'primary-bottom', hex: '#fc0000'},
+            {name:'primary-text', hex: '#FFFFFF'},
+            {name:'primary-border', hex: '#300b2b'},
+            {name:'primary-buttons', hex: '#000'},
+        ]
+    },
 ]
