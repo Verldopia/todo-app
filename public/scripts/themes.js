698541814bc57ecdd926c3c9ae8cbd28b97c6643
Michiel Willems <91065562+pgm-michielwillems@users.noreply.github.com>
2022-02-26 14:19:15 +0100

    added additional styling for themeswitch

--- public/scripts/themes.js
+++ public/scripts/themes.js
@@ -8,6 +8,7 @@ export const themes = [
             {name:'primary-text', hex: '#FFFFFF'},
             {name:'primary-border', hex: '#8b267e'},
             {name:'primary-buttons', hex: '#181818'},
+            {name:'switch-img', hex: 'url(../images/beach.png)'},
         ]
     },
     {
@@ -19,6 +20,7 @@ export const themes = [
             {name:'primary-text', hex: '#FFFFFF'},
             {name:'primary-border', hex: '#000000'},
             {name:'primary-buttons', hex: '#000000'},
+            {name:'switch-img', hex: 'url(../images/midnight.png)'},
         ]
     },
     {
@@ -30,17 +32,19 @@ export const themes = [
             {name:'primary-text', hex: '#ffff00'},
             {name:'primary-border', hex: '#21445B'},
             {name:'primary-buttons', hex: '#45214A'},
+            {name:'switch-img', hex: 'url(../images/toxic.png)'},
         ]
     },
     {
         name: 'Unicorn',
         slug: 'unicorn',
         colors: [
-            {name:'primary-top', hex: '#0011ff'},
-            {name:'primary-bottom', hex: '#fc0000'},
+            {name:'primary-top', hex: '#e65da1'},
+            {name:'primary-bottom', hex: '#22b8bd'},
             {name:'primary-text', hex: '#FFFFFF'},
-            {name:'primary-border', hex: '#300b2b'},
-            {name:'primary-buttons', hex: '#000'},
+            {name:'primary-border', hex: '#8b267e'},
+            {name:'primary-buttons', hex: '#8b267e'},
+            {name:'switch-img', hex: 'url(../images/unicorn.png)'},
         ]
     },
 ]
